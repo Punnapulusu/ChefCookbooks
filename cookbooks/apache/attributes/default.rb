b2dd53f1e68abe6a9b782a73ce5a2c6a446a25b0
PUNNA R PULUSU <32353874+Punnapulusu@users.noreply.github.com>
2017-10-30 02:29:35 -0700

    My Chef Cookooks

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["ip-172-31-33-125"] = { "site_title" => "Punna Website Comingsoon", "port" => 80, "domain" => "ip-172-31-33-125.us-east-2.compute.internal" }
+default["apache"]["sites"]["chefserver"] = { "site_title" => "Punna2 Website Comingsoon", "port" => 80, "domain" => "chefserver.us-east-2.compute.internal" }
